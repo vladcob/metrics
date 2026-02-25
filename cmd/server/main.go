e12a1c8262de764ee1ee3cbd9a2bad7af201332d
vkobza <v.kobza@unlimint.com>
2026-02-25 11:56:39 +0300

    increment 1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,89 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+)
+
+const (
+	GaugeType   = "gauge"
+	CounterType = "counter"
+)
+
+var Types = []string{
+	GaugeType,
+	CounterType,
+}
+
+type MemStorageInterface interface {
+	addCounter(name string, value int64)
+	addGauge(name string, value float64)
+	updateCounter(name string, value int64)
+	updateGauge(name string, value float64)
+	removeCounter(name string)
+	removeGauge(name string)
+}
+
+type MemStorage struct {
+	counters map[string]int64
+	gauges   map[string]float64
+}
+
+type Middleware func(http.Handler) http.Handler
+
+func main() {
+	http.Handle("/update/{type}/{name}/{value}", Conveyor(http.HandlerFunc(updateMetric), notFoundMiddleware, badRequestMiddleware))
+
+	err := http.ListenAndServe(`:8080`, nil)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func updateMetric(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Ok status"))
+}
+
+func Conveyor(h http.Handler, middlewares ...Middleware) http.Handler {
+	for _, middleware := range middlewares {
+		h = middleware(h)
+	}
+	return h
+}
+
+func notFoundMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if name := r.PathValue("name"); name == "" {
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte("Metric name not found"))
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func badRequestMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		isValid := false
+
+		for _, v := range Types {
+			if v == r.PathValue("type") {
+				isValid = true
+			}
+		}
+
+		if isValid {
+			next.ServeHTTP(w, r)
+		} else {
+
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Invalid metric type"))
+		}
+	})
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vladcob/metrics
+
+go 1.25.1
